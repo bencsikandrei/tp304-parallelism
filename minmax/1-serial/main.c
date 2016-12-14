41d489da205f67e418646e738f773a851fbfd7e2
Andrei Bencsik <andrei.bencsik@gmail.com>
2016-12-14 13:23:32 +0100

    One loop is worse than two

--- minmax/1-serial/main.c
+++ minmax/1-serial/main.c
@@ -3,7 +3,7 @@
 
 #include "bench.h"
 #include "minmax.h"
-
+// initialize all array values to a random number
 static void random_init(size_t n, uint32_t v[n]) {
 	for(size_t i=0;i<n; i++)
 		v[i] = rand();
@@ -15,25 +15,23 @@ int main(int argc, char** argv)
 		fprintf(stderr, "Usage: %s n\n", argv[0]);
 		return 1;
 	}
-	printf("Using one time read\n");
-
+	// covert the argument to a long long
 	const size_t n = atoll(argv[1]);
-
-	uint32_t (*vec)[n] = malloc( n*sizeof(uint32_t));
+	// allocate enough memory for as many ints as the user requires
+	uint32_t (*vec)[n] = malloc( n * sizeof(uint32_t));
     if(!vec) {
         fprintf(stderr, "failed to allocate %lu bytes\n", n*sizeof(uint32_t) );
         return 2;
     }
 
 	srand(0); // Constant values over different launches for "random" numbers
-	// random_init(n, *vec);
+	
+	random_init(n, *vec);
 
 	uint32_t min, max;
-	// start bench
+	// time the run and compute the throughput
 	BENCH_START(minmax);
-	// compute_minmax(n, *vec, &min, &max);
-	compute_minmax_random(n, *vec, &min, &max);
-	// bench
+	compute_minmax(n, *vec, &min, &max);
 	BENCH_END(minmax, "minmax", n, sizeof(uint32_t), 1, sizeof(uint32_t));
 
 	printf("min = %u / max = %u\n", min, max);
--- minmax/1-serial/minmax.h
+++ minmax/1-serial/minmax.h
@@ -5,6 +5,4 @@
 #include <stdlib.h>
 
 void compute_minmax(size_t n, uint32_t* const vec, uint32_t* min, uint32_t* max);
-void compute_minmax_random(size_t n, uint32_t* const vec, uint32_t* min, uint32_t* max);
-
 #endif
